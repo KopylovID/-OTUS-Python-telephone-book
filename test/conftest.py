b131ad6a62befbc08c1de19e635ed00dc27a0b8a
stratig <kopylovid@yandex.ru>
2025-11-29 15:06:19 +0300

    Тесты для модели: ins/upd/del

---
+++ test/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+from common.data import Data
+from typing import Tuple
+from common.contact import Contact
+from dataclasses import asdict
+from test.test_dataset import test_ds
+
+
+@pytest.fixture(scope="function")
+def data_empty() -> Data:
+    return Data()
+
+@pytest.fixture(scope="function", params=[test_ds])
+def data_filled(request) -> Data:
+    data: Data = Data()
+    for el in request.param:
+        contact = Contact(*el)
+        data.insert(asdict(contact))
+    return data
